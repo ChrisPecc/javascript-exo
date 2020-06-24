003ce653d8a73942f1a4c176c112e6e20e26d67f
ChrisPecc <pecceu.christophe@gmail.com>
2020-06-24 11:46:11 +0200

    fin ex 11.2

--- 11-fetch/02-list-to-template/script.js
+++ 11-fetch/02-list-to-template/script.js
@@ -11,4 +11,31 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        fetch("http://localhost:3000/heroes")
+        .then(response => response.json())
+        .then(xmen => original5 = Array.from(xmen))
+        .then(
+            
+            function putxmen(){
+                var template, item, a;
+                template = document.getElementById("tpl-hero");
+                item = template.content.querySelector("li");
+                for (i=0; i < original5.length; i++) {
+                    console.log(original5[i])
+                    a = document.importNode(item, true);
+                    a.querySelector("strong").textContent += original5[i].name;
+                    a.querySelector("em").textContent += original5[i].alterEgo;
+                    a.querySelector("p").textContent += original5[i].abilities;
+
+                    document.getElementById("target").appendChild(a);
+                }
+                
+            }
+        )
+        
+        
+
+
+    });
 })();
